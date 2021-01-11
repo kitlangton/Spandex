3087233c03c5786716e27380bb72db229502bcad
Kit Langton <kit.langton@gmail.com>
2021-01-10 17:24:50 -0800

    Initial Commit

---
+++ Spandex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spandex
+//
+//  Created by Kit Langton on 1/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spandex/SpandexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpandexApp.swift
+//  Spandex
+//
+//  Created by Kit Langton on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SpandexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
